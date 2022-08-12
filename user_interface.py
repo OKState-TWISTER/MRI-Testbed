85ea96cc0f9f6833f76fbe5a46d9db96e6184ac1
= <matotto@okstate.edu>
2022-08-12 13:06:19 -0500

    changed how settings handles modes

--- user_interface.py
+++ user_interface.py
@@ -1,3 +1,6 @@
+# This file is a mess and desperately needs to be rewritten.
+# Please do not edit unless you really know what you are doing.
+
 import csv
 import pathlib
 import os
@@ -5,35 +8,63 @@ import os
 settings_filename = "settings.csv"
 
 
-class IO:
+class UserSettings:
     def __init__(self, debug):
         self.debug = debug
+
+        self.mode = Setting("measurement mode", description="measure complex waveform errors or single-tone signal amplitude", valid_values=["cw", "ber"])
 
         self.test_series = Setting("test series", description="this will determine which folder the test data is saved to")
         self.desc = Setting("test description", description="this will be used to name the output files")
         self.starting_pos = Setting("starting angle", description="where the test should begin (positive)")
         self.ending_pos = Setting("ending angle", description="where the test should end (negative probably)")
         self.step_size = Setting("step size", description="how many degrees between each sample point")
-        self.averaging_time = Setting("averaging time", description="how many seconds to wait for DSO to average before recording value (this setting is ignored in SER mode)")
         self.zero_offset = Setting("zero offset", default_value="0", description="stage position that results in 0 degree actual angle for DUT.\n" +
                                    "!!! Leave at 0 unless you know what you're doing !!!")
-        self.mode = Setting("measurement mode", description="measure complex waveform errors or single-tone signal amplitude", valid_values=["ser", "amplitude"])
+        # Single tone
+        self.averaging_time = Setting("averaging time", description="how many seconds to wait for DSO to average before recording value")
+
+        # Modulated
+        self.save_waveforms = Setting("save waveforms", description="should every waveform capture be saved to a file for further analysis", valid_values=['t', 'f', 'y', 'n', '1', '0'], bool=True)
+       
+        
 
         self.settings_file = os.path.join(
             pathlib.Path(__file__).parent.absolute(), settings_filename
         )
 
-        settings = {
+        gen_settings = {
+            "mode": self.mode,
             "series": self.test_series,
             "starting_pos": self.starting_pos,
             "ending_pos": self.ending_pos,
             "step_size": self.step_size,
-            "averaging_time": self.averaging_time,
             "zero_offset": self.zero_offset,
-            "mode": self.mode,
         }
 
-        self.load(settings)
+        cw_settings = {
+            "averaging_time": self.averaging_time,
+        }
+
+        mt_settings = {
+            "save_waveforms": self.save_waveforms,
+        }
+
+        self.load(gen_settings)
+
+        mode = gen_settings["mode"]
+        if not mode.value:
+            self.get_input(self.mode)
+            mode = self.mode
+
+        if mode.value == "cw":
+            self.load(cw_settings)
+            settings = gen_settings | cw_settings
+        elif mode.value == "ber":
+            self.load(mt_settings)
+            settings = gen_settings | mt_settings
+            
+
 
         if None not in [setting.value for setting in settings.values()]:
             print("\nPreviously used settings:")
@@ -43,11 +74,23 @@ class IO:
                 '\nWould you like to edit these settings?\n(enter "y" to edit or nothing to continue): '
             )
             if choice.lower() == "y":
+                self.get_input(self.mode)
+                mode = self.mode
+
+                if mode.value == "cw":
+                    self.load(cw_settings)
+                    settings = gen_settings | cw_settings
+                elif mode.value == "ber":
+                    self.load(mt_settings)
+                    settings = gen_settings | mt_settings
+
                 for setting in settings.values():
-                    self.get_input(setting)
+                    if not setting.name == "measurement mode":
+                        self.get_input(setting)
         else:
             for setting in settings.values():
-                self.get_input(setting)
+                if not setting.name == "measurement mode":
+                    self.get_input(setting)
 
         self.get_input(self.desc)
 
@@ -91,7 +134,13 @@ class IO:
             nv = input(prompt_text)
             if nv:
                 if (setting.valid_values and nv in setting.valid_values) or not setting.valid_values:
-                    setting.value = nv
+                    if setting.bool:
+                        if nv.lower() in ("y", "yes", "t", "true", "1"):
+                            setting.value = "true"
+                        else:
+                            setting.value = "false"
+                    else:
+                        setting.value = nv
                 elif setting.valid_values:  # and nv not in setting.valid_values
                     continue
 
@@ -100,11 +149,12 @@ class IO:
 
 
 class Setting:
-    def __init__(self, name=None, default_value=None, valid_values=None, description=None):
+    def __init__(self, name=None, default_value=None, valid_values=None, description=None, bool=None):
         self.name = name
         self.value = default_value
         self.valid_values = valid_values
         self.desc = description
+        self.bool = bool
 
     def __call__(self):
         return self.value
@@ -112,6 +162,5 @@ class Setting:
     def __repr__(self):
         return self.value
 
-
 if __name__ == '__main__':
-    IO(True)
+    UserSettings(True)
