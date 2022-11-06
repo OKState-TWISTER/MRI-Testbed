ad1c263e727fb758c90d31ee4082c67a10f2f11d
Matthew Otto <matotto@okstate.edu>
2022-11-06 11:00:04 -0600

    v3 rewrite

--- characterize_vdi.py
+++ characterize_vdi.py
@@ -1,17 +1,15 @@
-# v2.7c
+# v3.0b1
 
 """
 This program serves to automatically profile VDI modules by controlling various components:
-A Thorlabs HDR50 rotator stage connected to a BSC201 controller positions the TX antenna
 A Keysight DSOV254A oscilloscope captures received waveforms
 The captured waveforms are analyzed using various MATLAB functions
+
+Requires TWISTER Automation Library:
+pip install git+https://github.com/OKState-TWISTER/TWISTER-Automation-Library
 """
 
-# TODO:
-# control speed of rotator
 
-# import libraries
-from array import array
 import csv
 import datetime
 import os
@@ -21,199 +19,101 @@ import time
 import matplotlib.pyplot as plt
 import pickle
 
-from fileio import File_IO
+from oscilloscope_interface import Oscilloscope
+from waveformgen_interface import WaveformGenerator
+from signalgen_interface import SignalGenerator
+import twister_utils
+
+import fileio
 from plot import Custom_Plot
-from stage_control import Kinesis
-from scope_control import Infiniium
 from user_interface import UserSettings
 from waveform_analysis import WaveformProcessor
 
 
 # Global Variables
-serial_num = "40163084"  # Serial number for rotation stage
-visa_address = "USB0::0x2A8D::0x9027::MY59190106::0::INSTR"  # VISA address for DSO
-
 date_time = datetime.datetime.now().strftime("%Y-%m-%dT%H%M%z")
 output_dir = "Data"
 
 # Various mode controls
 debug = True  # prints a whole lot of debug info
-ignore_rotator = True  # performs a single shot measurement (ignores rotation stage), dumps waveform to file
 
 
 def main():
-    if ignore_rotator:
-        print("\nWarning: Rotation stage disabled\n")
-
-    settings = UserSettings(debug)  # prompt user for settings
-
-    mode = settings.mode()
-    starting_angle = float(settings.starting_pos())
-    ending_angle = float(settings.ending_pos())
-    step_size = float(settings.step_size())
-    zero_offset = float(settings.zero_offset())
-    save_waveforms = settings.save_waveforms() == "true"
-
-    # TODO: have UserSettings handle casting types
-    if mode == "cw":
-        averaging_time = float(settings.averaging_time())
-    if mode == "ber":
-        if_estimate = float(settings.if_estimate())
-        if save_waveforms:
-            waveform_count = int(settings.waveform_count())
+    test_series = ""
+    description = ""  # optional
+    capture_count = 5  # number of captures to save from the scope per source waveform
+    if_estimate = ""  # TODO: is this necessary to save?
 
     # Create save destination
-    global save_dir
-    save_dir = os.path.join(output_dir, os.path.normpath(settings.test_series()))
+    save_dir = os.path.join(output_dir, os.path.normpath(f"{test_series}_{date_time}"))
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    global destination_filename
-    destination_filename = f"{settings.desc}_{date_time}"
 
-    if save_waveforms:
-        waveform_dir = os.path.join(save_dir, destination_filename + "_waveforms")
-        fileio = File_IO(waveform_dir)
 
-    # Initialize DSO (scope_control.py)
-    scope = Infiniium(visa_address, debug)
+    # Initialize Instruments
+    scope = Oscilloscope(debug=debug)
+    awg = WaveformGenerator(debug=debug)
+    psg1 = SignalGenerator(1, debug=debug)
+    psg2 = SignalGenerator(2, debug=debug)
 
-    if not ignore_rotator:
-        # Initialize rotation stage (stage_control.py)
-        stage = Kinesis(serial_num, debug, starting_angle, zero_offset)
+    while True:
+        response = input("Are the VDI modules powered on? (y/N): ")
+        if response.lower() == "y":
+            break
+        else:
+            print("Please ensure the VDI modules are powered on before enabling the LO and AWG outputs.\n")
 
-    if mode == "ber":
-        # Initialize MATLAB Engine (waveform analysis.py)
-        waveform_proc = WaveformProcessor(if_estimate=if_estimate, debug=True) #debug=not save_waveforms
 
-    if not ignore_rotator:
-        if not stage.home():
-            print("Error when homing stage")
-            sys.exit(-1)
 
-    # TODO: have an updating estimation for remaining time
-    #test_length = (starting_angle - ending_angle) / step_size * averaging_time / 60
-    #print(f"Test will take {test_length} minutes to complete.")
-
-    input("Press any key to begin")
-
-    # Write test info to file
-    info_fp = os.path.join(save_dir, f"{destination_filename}.info")
+    info_fp = os.path.join(save_dir, "info.txt")
     with open(info_fp, 'w') as file:
-        file.write(f"Description: {settings.desc}\n")
-        file.write(f"Mode: {mode}\n")
-        file.write(f"Rotation span: {starting_angle} - {ending_angle} degrees\n")
-        file.write(f"Step size: {step_size} degrees\n")
-        if mode == "cw":
-            file.write(f"Averaging time: {averaging_time} seconds\n")
-        if mode == "ber":
-            file.write(f"# waveforms per position: {waveform_count}\n")
-            file.write(f"Waveform IF estimate: {if_estimate}\n")
-            file.write(f"Original waveform filename: {waveform_proc.original_waveform}\n")
+        file.writelines([
+            f"Test Series: {test_series}\n",
+            f"Description: {description}\n",
+            f"# captures per waveform: {capture_count}\n",
+            f"Waveform IF estimate: {if_estimate}\n",
+        ])
+
+    
+    # Generate source waveforms (if option is selected)
+    source_waveform_dir = os.path.join(save_dir, "source_waveforms")
+    if not os.path.exists(source_waveform_dir):
+        os.makedirs(source_waveform_dir)
+    # for [parameter]
+    # dump generated files in source_waveform_dir
+
+    #else select path to source waveforms
 
 
-    # Begin Test
-    scope.do_command(":CDISplay")
-    try:
-        if not ignore_rotator:
-            plot = Custom_Plot(settings.desc, mode, save_dir, destination_filename)
-            current_pos = stage.get_rel_angle()
-            data = [[], []]
+    #created captured waveform directory
+    captured_waveform_dir = os.path.join(save_dir, "captured_waveforms")
+    if not os.path.exists(captured_waveform_dir):
+        os.makedirs(captured_waveform_dir)
 
-            while current_pos >= ending_angle:
-                print(f"Stage position: {current_pos} (absolute) {current_pos - zero_offset} (effective)")
 
-                if mode == "cw":
-                    datapoint = measure_amplitude(scope, averaging_time, save_wf=save_waveforms)
-                    if save_waveforms:
-                        datapoint, waveform, samp_rate = datapoint
-                        fileio.save_waveform(waveform, samp_rate, current_pos, 1)
-                elif mode == "ber":
-                    datapoint = 0
-                    n = waveform_count
-                    while True:
-                        (ber, waveform, samp_rate) = measure_ber(scope, waveform_proc)
+    #### Run the tests ####
+    # Enable Signal Generators and then AWG output
+    with psg1.enable_output(), psg2.enable_output(), awg.enable_output():
+        for file in os.listdir(source_waveform_dir):
+            if not file.endswith(".bin"):
+                continue
+            sourcefilepath = os.path.join(source_waveform_dir, file)
+            
+            awg.load_waveform(sourcefilepath, sample_rate)
 
-                        if not save_waveforms:
-                            datapoint = ber
-                            break
-                        else:
-                            datapoint += ber
-                            fileio.save_waveform(waveform, samp_rate, current_pos, n)
-                            n -= 1
-                            if n == 0:
-                                datapoint = datapoint / waveform_count
-                                break
+            for n in range(1, capture_count+1):  # one-based index
+                data = scope.get_waveform_bytes(channels=1)
+                scope_sr = scope.get_sample_rate()
 
-                data[0].append(stage.get_abs_angle() - zero_offset)
-                data[1].append(datapoint)
+                outfilepath = os.path.join(captured_waveform_dir, f"{file.replace('.bin', '')}_{n}")
+                fileio.save_waveform(data, scope_sr, outfilepath)
 
-                plot.update(data)
-
-                print("Stage is moving")
-                # Be very careful when moving the stage to not wrap coax
-                stage.move_to(current_pos - step_size)
-                current_pos = stage.get_rel_angle()
-
-            data_dest = os.path.join(save_dir, destination_filename + ".csv")
-            print(f"writing data to {data_dest}")
-            with open(data_dest, "w", newline="") as csvfile:
-                csvwriter = csv.writer(csvfile)
-                for i in range(len(data[0])):
-                    row = (data[0][i], data[1][i])
-                    csvwriter.writerow(row)
-
-            plot.print_report()
-
-        else:  # if ignore_rotator mode
-            if mode == "cw":
-                data = measure_amplitude(scope, averaging_time, save_wf=save_waveforms)
-                if save_waveforms:
-                    data, waveform, samp_rate = data
-                    fileio.save_waveform(waveform, samp_rate, None, 1)
-                
-            elif mode == "ber":
-                datapoint = 0
-                n = waveform_count
-                first = True
-                while True:
-                    (ber, waveform, samp_rate) = measure_ber(scope, waveform_proc, first)
-                    first = False
-
-                    if not save_waveforms:
-                        datapoint = ber
-                        break
-                    else:
-                        datapoint += ber
-                        fileio.save_waveform(waveform, samp_rate, None, n)
-                        n -= 1
-                        if n == 0:
-                            datapoint = datapoint / waveform_count
-                            break
-                print(f"Average BER: {datapoint}")
-
-    except KeyboardInterrupt:
-        pass
 
     input("Test complete.")
 
 
 ###############################################################################
 
-
-def measure_amplitude(scope, averaging_time, save_wf=False):
-    # Reset averaging
-    scope.do_command(":CDISplay")
-    time.sleep(averaging_time)
-    datapoint = scope.get_fft_peak()
-    # TODO: make sure peak is at correct frequency
-    print(f"Power level: {datapoint} dBm")
-
-    if save_wf:
-        waveform = scope.get_waveform_words()
-        samp_rate = scope.get_sample_rate()
-        return (datapoint, waveform, samp_rate)
-
-    return datapoint
 
 
 def measure_ber(scope, waveform_proc, analyze):
--- fileio.py
+++ fileio.py
@@ -2,90 +2,76 @@ from array import array
 import pickle
 import os
 
+# TODO: save only required number of bytes per sample
 
-class File_IO:
-    def __init__(self, waveform_dir):
-        self.waveform_dir = waveform_dir
+def save_waveform(self, waveform, samp_rate, filepath):
+    if not position:
+        position = "static"
+    datafile = os.path.join(self.waveform_dir, f"{position}_{n}")
 
+    version = 1
+    ver = version.to_bytes(2, 'big')
+    sample_rate = int(samp_rate).to_bytes(8, 'big')
+    num_samples = len(waveform).to_bytes(8, 'big')
+    data = array('i', waveform).tobytes()
 
-    def save_waveform(self, waveform, samp_rate, position, n):
-        if not os.path.exists(self.waveform_dir):
-            os.makedirs(self.waveform_dir)
-
-        if not position:
-            position = "static"
-        datafile = os.path.join(self.waveform_dir, f"{position}_{n}")
-
-        version = 1
-        ver = version.to_bytes(2, 'big')
-        sample_rate = int(samp_rate).to_bytes(8, 'big')
-        num_samples = len(waveform).to_bytes(8, 'big')
-        data = array('i', waveform).tobytes()
-
-        print(f"Saving {num_samples} samples at {samp_rate} Samp/sec to file '{datafile}' using v{version} formatting")
-        with open(datafile, 'wb') as outp:
-            outp.write(ver)
-            outp.write(sample_rate)
-            outp.write(num_samples)
-            outp.write(data)
+    print(f"Saving {num_samples} samples at {samp_rate} Samp/sec to file '{datafile}' using v{version} formatting")
+    with open(datafile, 'wb') as outp:
+        outp.write(ver)
+        outp.write(sample_rate)
+        outp.write(num_samples)
+        outp.write(data)
 
 
-    def load_waveform(self, filename):
-        filepath = os.path.join(self.waveform_dir, filename)
-        
-        # If data is pickled
-        if filepath.endswith(".pkl"):
-            return self.load_pickle(filepath)
+def load_waveform(self, filename):
+    filepath = os.path.join(self.waveform_dir, filename)
+    
+    # If data is pickled
+    if filepath.endswith(".pkl"):
+        return self.load_pickle(filepath)
 
-        # Else, get format version number from binary file
-        with open(filepath, 'rb') as file:
-            version = int.from_bytes(file.read(2), 'big')
+    # Else, get format version number from binary file
+    with open(filepath, 'rb') as file:
+        version = int.from_bytes(file.read(2), 'big')
 
-        # Load the file depending on the format
-        if version == 1:
-            return self.load_bin1(filepath)
-        if version == 2:
-            pass
-
-
-    def load_pickle(self, filepath):
-        with open(filepath, 'rb') as inp:
-            samp_rate = pickle.load(inp)
-            num_samples = pickle.load(inp)
-            waveform = pickle.load(inp)
-
-        return (samp_rate, num_samples, waveform)
+    # Load the file depending on the format
+    if version == 1:
+        return self.load_bin1(filepath)
+    if version == 2:
+        pass
 
 
-    def load_bin1(self, filepath):
-        ## [bytes] - description ##
-        # [0:1] - version (2 bytes)
-        # [2:9] - samplerate (8 bytes)
-        # [10:17] - sample count (8 bytes)
-        # [18:] - samples (n bytes)
-        
-        data = array('i')
+def load_pickle(self, filepath):
+    with open(filepath, 'rb') as inp:
+        samp_rate = pickle.load(inp)
+        num_samples = pickle.load(inp)
+        waveform = pickle.load(inp)
 
-        with open(filepath, 'rb') as inp:
-            version = int.from_bytes(inp.read(2), 'big')
-            samp_rate = int.from_bytes(inp.read(8), 'big')
-            samp_count = int.from_bytes(inp.read(8), 'big')
-            data.frombytes(inp.read())
-        
-        samples = data.tolist()
+    return (samp_rate, num_samples, waveform)
 
-        if len(data) != samp_count:
-            print("Warning: loading file error.\nPayload length does not match sample count. Data may be corrupted")
 
-        return (samp_rate, samp_count, samples)
+def load_bin1(self, filepath):
+    ## [bytes] - description ##
+    # [0:1] - version (2 bytes)
+    # [2:9] - samplerate (8 bytes)
+    # [10:17] - sample count (8 bytes)
+    # [18:] - samples (n bytes)
+    
+    data = array('i')
+
+    with open(filepath, 'rb') as inp:
+        version = int.from_bytes(inp.read(2), 'big')
+        samp_rate = int.from_bytes(inp.read(8), 'big')
+        samp_count = int.from_bytes(inp.read(8), 'big')
+        data.frombytes(inp.read())
+    
+    samples = data.tolist()
+
+    if len(data) != samp_count:
+        print("Warning: loading file error.\nPayload length does not match sample count. Data may be corrupted")
+
+    return (samp_rate, samp_count, samples)
 
 
 if __name__ == '__main__':
-    io = FileIO("TestData", "")
-    #data = [123534, -12455, 124532, 748, -10]
-
-    #io.save_waveform(data, 80000000000, None, 1)
-
-    data = io.load_waveform("static_1")
-
-    print()
+    pass
