97a4e359faa71a3120c0db327ec642b11c7ed246
Tyler Graham <35854538+TylerGraham74@users.noreply.github.com>
2023-09-26 11:24:59 -0500

    Update characterize_horn_antenna.py

--- characterize_horn_antenna.py
+++ characterize_horn_antenna.py
@@ -1,12 +1,44 @@
-import motion_stage.py
+import motion_stage
+import time
+import numpy as np
 
-def characterize(min_angle, max_angle, step_size):
-    motion_stage.setup()
-    motion_stage.home(motion_stage.motor)
-    motion_stage.set_pos(motion_stage.motor,min_angle)
+from twister_api.oscilloscope_interface import Oscilloscope
+scope = Oscilloscope(debug=True)
+
+#parameters: start angle (degrees), end angle (in degrees), step size (in degrees), sleep time (in ms)
+def characterize(min_angle, max_angle, step_size, pause_time):
+    #arrays to keep track of current angle and fft_peak measurements
+    angles = np.array([])
+    fft_peaks = np.array([])
+
+    #motor setup and homing
+    motor = motion_stage.setup()
+    motion_stage.home(motor)
+
+    #convert angles to device measurements
+    min_angle = angle_to_device_units(min_angle)
+    max_angle = angle_to_device_units(max_angle)
+    step_size = angle_to_device_units(step_size)
+
+    #step the specified size and take fft peak measurent repeatedly until rotated desired amount
     curr_angle = min_angle
     while curr_angle <= max_angle:
-        motion_stage.set_pos(motion_stage.motor,curr_angle)
+        motion_stage.set_pos(motor,curr_angle)
         curr_angle += step_size
-        motion_stage.wait(1,motion_stage.motor)
+        time.sleep(pause_time * 1000)
+
         #Capture oscilloscope measurements
+        peak = scope.get_fft_peak()
+        angles = np.append(angles, curr_angle)
+        fft_peaks = np.append(fft_peaks, peak)
+
+    #save data in csv format
+    arr = np.asarray([peak, angles])
+    np.savetxt('horn_anntena_characterization.csv', arr, fmt='%d', delimiter=",")
+
+#convert angle degree measurements to device units
+def angle_to_device_units(angle):
+    return int(angle/0.00001331666666667)
+
+class characterize_horn_antenna:
+    characterize(0, 90, 1, 1000)
